ebd589dffd481f5a920ac75c968f371ba0a59d36
sieuphan <phanthanhsieu131102@gmail.com>
2022-11-20 22:18:59 +0700

    #5 - update style chitietsp

---
+++ README.md
@@ -0,0 +1,15 @@
+# Giới thiệu
+
+**Foodysor** là một web app mang đến cho người dùng những loại hamburger hảo hạng.
+
+***
+
+## Folder trong dự án
+
+**assets**: nơi chứa các tệp hình ảnh, video...\
+**pages**: nơi chứa các trang trong web (HTML).\
+**script**: nơi chứa các việc xử lý trong JavaScript.\
+**styles**: nơi chứa các tệp css (nếu như sử dụng Tailwind, vui lòng bỏ qua folder này).
+
+## Cấu hình để sử dụng 
+- Cần cài đặt Framework Tailwind để mang đến trải nghiệm tốt nhất.
---
+++ db/blogs.js
@@ -0,0 +1,55 @@
+const blogList = [
+    {
+        id: "1",
+        title: "MUA 1 TẶNG 1 - THỨ 5 HÀNG TUẦN",
+        img: "../assets/images/banner-trang-khuyen-mai_1_.jpg",
+        time: "Last friday at 10:34 pm",
+    },
+    {
+        id: "2",
+        title: "MUA 1 TẶNG 1 - THỨ 5 HÀNG TUẦN",
+        img: "../assets/images/banner-trang-khuyen-mai_1_.jpg",
+        time: "Last friday at 10:34 pm",
+    },
+    {
+        id: "3",
+        title: "MUA 1 TẶNG 1 - THỨ 5 HÀNG TUẦN",
+        img: "../assets/images/banner-trang-khuyen-mai_1_.jpg",
+        time: "Last friday at 10:34 pm",
+    },
+    {
+        id: "4",
+        title: "MUA 1 TẶNG 1 - THỨ 5 HÀNG TUẦN",
+        img: "../assets/images/banner-trang-khuyen-mai_1_.jpg",
+        time: "Last friday at 10:34 pm",
+    },
+    {
+        id: "5",
+        title: "MUA 1 TẶNG 1 - THỨ 5 HÀNG TUẦN",
+        img: "../assets/images/banner-trang-khuyen-mai_1_.jpg",
+        time: "Last friday at 10:34 pm",
+    },
+    {
+        id: "6",
+        title: "MUA 1 TẶNG 1 - THỨ 5 HÀNG TUẦN",
+        img: "../assets/images/banner-trang-khuyen-mai_1_.jpg",
+        time: "Last friday at 10:34 pm",
+    },
+];
+
+const row2 = document.querySelector(".rows-2");
+
+row2.innerHTML = blogList
+    .map(
+        (
+            item
+        ) => `<a href="./chiTietKhuyenMai.html" class="flex justify-center m-4 z-[1]">
+                        <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                            <img class="rounded-t-lg" src=${item.img} alt="../assets/images/banner-trang-khuyen-mai_1_.jpg" loading="lazy"/>
+                        <div class="p-6">
+                            <h5 class="text-gray-900 text-xl font-bold mb-2">${item.title}</h5>                            
+                        </div>
+                        </div>
+                    </a>`
+    )
+    .join("");
